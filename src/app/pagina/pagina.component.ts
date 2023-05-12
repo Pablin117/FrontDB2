edeb5036cc2171e7cf1b646c75a4fbe42d4c6ecd
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-05-12 12:02:15 -0600

    cambios para que funcione tabla y cuadro

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -1,8 +1,6 @@
-import {Component, OnInit,} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
 import axios from 'axios';
-import {catchError} from "rxjs";
 
 @Component({
   selector: 'app-pagina',
@@ -12,7 +10,6 @@ import {catchError} from "rxjs";
 export class PaginaComponent {
   title = 'DB2';
 
-
   public columns: string[] = [];
   public rows: any;
   private json: any;
@@ -20,36 +17,82 @@ export class PaginaComponent {
 
 
   constructor(private http: HttpClient) {
+
   }
 
   ngOnInit(): void {
-
- this.makeRequest()
-    this.funcion2();
+    this.funcionTabla()
   }
 
-  async makeRequest() {
-    console.log("soi");
+   clearOutput() {
+     const tableBody = document.getElementById('table-body');
+     const resultadoElement = document.getElementById('resultado');
+    if (tableBody != null) {
+      tableBody.innerHTML = '';
+    }
+     if (resultadoElement != null) {
+       resultadoElement.innerHTML = '';
+     }
+  }
+
+  async funcionTabla() {
+
     const inputText = document.getElementById('input-text') as HTMLInputElement;
     const sendButton = document.getElementById('send-button');
+    const tableBody = document.getElementById('table-body');
     const resultadoElement = document.getElementById('resultado');
 
-    if (sendButton != null && resultadoElement != null) {
+
+    if (sendButton != null) {
       sendButton.addEventListener('click', async () => {
         const sentencia = inputText.value;
         try {
-          const response = await axios.post('http://localhost:4043/custom-table', { sentencia });
-          console.log(response.data);
-          if (response.data == "e") {
-            alert("No hay servicio");
-          } else {
-            if (typeof response.data === 'string') {
-              // La respuesta es un string
-              console.log(response.data);
+          const response = await axios.post('http://localhost:4043/custom-table', {sentencia});
+          if (typeof response.data === 'object') {
+            console.log("entro en object")
+            // La respuesta es un objeto JSON
+
+
+
+
+            if (tableBody != null ) {
+              // Limpiar el contenido de la tabla
+              tableBody.innerHTML = '';
+              console.log("entro")
+
+              const info = response.data;
+              if (info.length > 0) {
+                // Obtener los campos presentes en los objetos de la respuesta
+                const fields = Object.keys(info[0]);
+                // Crear las columnas de la tabla
+                const headerRow = document.createElement('tr');
+                fields.forEach(field => {
+                  const headerCell = document.createElement('th');
+                  headerCell.textContent = field;
+                  headerRow.appendChild(headerCell);
+                });
+                tableBody.appendChild(headerRow);
+                // Iterar sobre los objetos del array y agregar filas a la tabla
+                info.forEach((item: any) => {
+                  const row = document.createElement('tr');
+                  fields.forEach(field => {
+                    const cell = document.createElement('td');
+                    const value = item[field];
+                    if (typeof value === 'object') {
+                      cell.textContent = JSON.stringify(value);
+                    } else {
+                      cell.textContent = value;
+                    }
+                    row.appendChild(cell);
+                  });
+                  tableBody.appendChild(row);
+                });
+              }
+            }
+          } else if (typeof response.data === 'string') {
+
+            if (resultadoElement != null) {
               resultadoElement.innerHTML = response.data;
-            } else if (typeof response.data === 'object') {
-              // La respuesta es un objeto JSON
-              console.log(response.data);
             }
           }
         } catch (error) {
@@ -60,54 +103,5 @@ export class PaginaComponent {
     }
   }
 
-  async funcion2() {
-    const inputText = document.getElementById('input-text') as HTMLInputElement;
-    const tableBody = document.getElementById('table-body');
-
-    const sendButton = document.getElementById('send-button');
-    if (sendButton != null) {
-      sendButton.addEventListener('click', async () => {
-        const sentencia = inputText.value;
-
-        try {
-          const response = await this.http.post('http://localhost:4043/custom-table', { sentencia }).toPromise();
-          console.log(response);
-
-          if (tableBody) {
-            // Limpiar el contenido de la tabla
-            tableBody.innerHTML = '';
-
-            const info = response && response && response;
-            if (info && Array.isArray(info) && info.length > 0) {
-              // Obtener los campos presentes en los objetos de la respuesta
-              const fields = Object.keys(info[0]);
-
-              // Crear las columnas de la tabla
-              const headerRow = document.createElement('tr');
-              fields.forEach(field => {
-                const headerCell = document.createElement('th');
-                headerCell.textContent = field;
-                headerRow.appendChild(headerCell);
-              });
-              tableBody.appendChild(headerRow);
-
-              // Iterar sobre los objetos del array y agregar filas a la tabla
-              info.forEach(item => {
-                const row = document.createElement('tr');
-                fields.forEach(field => {
-                  const cell = document.createElement('td');
-                  cell.textContent = item[field];
-                  row.appendChild(cell);
-                });
-                tableBody.appendChild(row);
-              });
-            }
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      });
-    }
-  }
-
 }
+
