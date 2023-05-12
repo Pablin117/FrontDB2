a4c922f9dcc2995a837ea3d51111c2a79e9e997e
Pablin117 <tgelma117@gmail.com>
2023-05-11 21:11:47 -0600

    paginat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit,  } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import { HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,69 +7,4 @@ import { HttpHeaders } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'DB2';
-
-  mensaje: String = "prueba";
-  prueba: string = "Ingrese un nombre";
-  mostrar: boolean =false;
-  user: any= {};
-  usuarioInvalido: boolean=false;
-
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    localStorage.clear()
-  }
-
-  mostrarMensaje(){
-    this.mostrar = !this.mostrar;
-  }
-
-  formulariologin(){
-
-    let formularioValido : any = document.getElementById("loginForm");
-    if(formularioValido.reportValidity()){
-      this.servicioLogin().subscribe(
-        (respuesta:any)=> this.login(respuesta)
-      )
-    }
-  }
-
-  login(res:any){
-    console.log(res)
-    if(res.length == 0){
-      this.usuarioInvalido=true;
-      console.log("paso por null")
-    }
-    else if(res=="e"){
-      alert("No hay comunicación con el servidor!!")
-    }
-    else if(res!=null){
-      localStorage.setItem("user",JSON.stringify(res));
-      location.href="/home";
-    }
-  }
-
-
-  crearUsuario(){
-    location.href="/user-creation";
-  }
-
-  servicioLogin(){
-    var httpOptions={
-      headers:new HttpHeaders({
-        'Content-Type':'application/json'
-      })
-    }
-
-    return this.http.post<any>("http://localhost:4042/custom-table", this.user, httpOptions).pipe(
-      catchError(e=>"e")
-    )
-
-  }
-
-  createuser(user:any){
-    localStorage.setItem("user",JSON.stringify(user));
-    location.href="/home";
-  }
 }
