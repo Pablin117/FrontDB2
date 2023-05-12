3a43658e81994bc37ae5f69e619b010b5a4fb9ec
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-05-12 09:30:02 -0600

    control de exception

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit,} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 import axios from 'axios';
+import {catchError} from "rxjs";
 
 @Component({
   selector: 'app-pagina',
@@ -22,35 +23,37 @@ export class PaginaComponent {
   }
 
   ngOnInit(): void {
-    this.funcion()
 
+ this.makeRequest()
   }
 
-  funcion() {
-    console.log("soi")
+  async makeRequest() {
+    console.log("soi");
     const inputText = document.getElementById('input-text') as HTMLInputElement;
     const sendButton = document.getElementById('send-button');
     const resultadoElement = document.getElementById('resultado');
 
     if (sendButton != null && resultadoElement != null) {
-
       sendButton.addEventListener('click', async () => {
-
-        const sentencia = inputText.value
-        const response = await axios.post('http://localhost:4043/custom-table', {sentencia});
-
-        if (response.data == "e") {
-          alert("No hay servicio")
-        } else {
-          if (typeof response.data === 'string') {
-            // La respuesta es un string
-            console.log(response.data);
-            resultadoElement.innerHTML = response.data
-          } else if (typeof response.data === 'object') {
-            // La respuesta es un objeto JSON
-            console.log(response.data)
-
+        const sentencia = inputText.value;
+        try {
+          const response = await axios.post('http://localhost:4043/custom-table', { sentencia });
+          console.log(response.data);
+          if (response.data == "e") {
+            alert("No hay servicio");
+          } else {
+            if (typeof response.data === 'string') {
+              // La respuesta es un string
+              console.log(response.data);
+              resultadoElement.innerHTML = response.data;
+            } else if (typeof response.data === 'object') {
+              // La respuesta es un objeto JSON
+              console.log(response.data);
+            }
           }
+        } catch (error) {
+          console.error(error);
+          alert("Debes ingresar una sentencia");
         }
       });
     }
