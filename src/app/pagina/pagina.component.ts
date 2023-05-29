b329e1d8fb3a8d83fc69e3a22544afbb5405190a
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-05-28 18:06:22 -0600

    cambios de login de ricardo

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -9,8 +9,7 @@ import axios from 'axios';
 })
 export class PaginaComponent {
   public itemsArray: any;
-  user: any = {};
-  pass: string = ""
+
 
 
   constructor(private http: HttpClient) {
@@ -18,7 +17,7 @@ export class PaginaComponent {
 
   ngOnInit(): void {
     this.funcionTabla()
-    this.funcionModal()
+    this.funcionModal1()
     localStorage.clear()
 
   }
@@ -27,6 +26,8 @@ export class PaginaComponent {
     const inputText = document.getElementById('input-text') as HTMLInputElement;
     const sendButton = document.getElementById('send-button');
     const resultadoElement = document.getElementById('resultado');
+
+    console.log(inputText)
     if (sendButton != null) {
       sendButton.addEventListener('click', async () => {
         const sentencia = inputText.value;
@@ -82,16 +83,27 @@ export class PaginaComponent {
 
 
   async funcionModal() {
-    const modal = document.getElementById('Modal');
     const btn = document.getElementById('abrirModal');
     const closeBtn = document.getElementsByClassName('close')[0] as HTMLElement;
-    const sendButton = document.getElementById('send-buttonU');
+    const modal = document.getElementById('Modal');
 
     if (btn) {
       btn.onclick = function () {
         if (modal) {
           modal.style.display = 'block';
-          console.log("entro ")
+
+          const sendButton = document.getElementById('send-buttonU');
+          const UserInput = document.getElementById('user-text') as HTMLInputElement;
+          const passwordInput = document.getElementById('password-text') as HTMLInputElement;
+          const esquemaInput = document.getElementById('esquema-text') as HTMLInputElement;
+
+
+          const user = UserInput.value;
+          const password = passwordInput.value;
+          const esquema = esquemaInput.value;
+
+
+
           if (sendButton != null) {
             sendButton.addEventListener('click', async () => {
               try {
@@ -99,15 +111,14 @@ export class PaginaComponent {
                   url: 'dbOS2', username: 'root', password: 'cristian13'
                 });
                 const info = response.data;
-                console.log(info)
-
-
+                const message: string = info.message;
+                alert(message)
+                modal.style.display = 'none';
               } catch (error) {
                 console.error(error);
                 alert("No hay comunicación con el servidor");
               }
             });
-
           }
         }
       };
@@ -123,9 +134,51 @@ export class PaginaComponent {
     window.onclick = function (event) {
       if (modal && event.target === modal) {
         modal.style.display = 'none';
-        console.log("windo")
       }
     }
+  }
+
+  async  funcionModal1() {
+    const btn = document.getElementById('abrirModal');
+    const closeBtn = document.getElementsByClassName('close')[0] as HTMLElement;
+    const modal = document.getElementById('Modal');
+
+    if (btn) {
+      btn.onclick = function () {
+        if (modal) {
+          modal.style.display = 'block';
+
+          const sendButton = document.getElementById('send-buttonU');
+
+          if (sendButton) {
+            sendButton.addEventListener('click', async () => {
+              const UserInput = document.getElementById('user-text') as HTMLInputElement;
+              const passwordInput = document.getElementById('password-text') as HTMLInputElement;
+              const esquemaInput = document.getElementById('esquema-text') as HTMLInputElement;
+
+              const user = UserInput.value;
+              const password = passwordInput.value;
+              const esquema = esquemaInput.value;
+
+              try {
+                const response = await axios.post('http://localhost:4043/check-db', {
+                  url: 'dbOS2', username: user, password: password
+                });
+                const info = response.data;
+                const message: string = info.message;
+                alert(message)
+                modal.style.display = 'none';
+              } catch (error) {
+                console.error(error);
+                alert("No hay comunicación con el servidor");
+              }
+            });
+          }
+        }
+      };
+    }
+
+    // Resto del código...
   }
 
 
